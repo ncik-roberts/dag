aa70f4e7852b2f9db1efd6109157268f170cb08f
ncik-roberts <nroberts@andrew.cmu.edu>
2018-11-15 17:07:47 -0500

    Make it better.

--- dag/dag_traversal.mli
+++ dag/dag_traversal.mli
@@ -1,5 +1,7 @@
 (** Topological sort of dag. *)
-type traversal_tree = Block of Dag.Vertex.t * traversal
+type traversal_tree =
+  | Block of Dag.Vertex.t * traversal
+  | Just of Dag.Vertex.t
 and traversal = traversal_tree list [@@deriving sexp]
 
 val any_traversal : Dag.dag -> traversal

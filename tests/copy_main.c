ce222f0825764461883e9c74c9971af6e9dfd366
Dan Cascaval <dcascava@andrew.cmu.edu>
2018-12-10 13:03:55 -0500

    compile things the way NVCC wants them compiled

--- tests/copy_main.c
+++ tests/copy_main.c
@@ -1,25 +1,22 @@
 #include "copy_test.h"
 
-int* copy(int* m1, int n)
+void copy(int* m3, int* m1, int n)
 {
-  int* m3 = (int*) malloc(n*n*sizeof(int));
   for (int i = 0; i < n; i++){
     m3[i] = m1[i];
   }
-  return m3;
 }
 
 int main(){
-  int NUM_ELEMS = 1 << 10; // 1024 to start. 
-  int len = NUM_ELEMS*NUM_ELEMS;
-  int* m3_dag  = (int*) malloc(NUM_ELEMS*NUM_ELEMS*sizeof(int));
+  int NUM_ELEMS = 4; // 1024 to start. 
 
-  int* m1 = initRandomArrayi(len);
-  int* m2 = initRandomArrayi(len);
+  int* m3_dag  = (int*) calloc(NUM_ELEMS,sizeof(int));
+  int* m3_c    = (int*) calloc(NUM_ELEMS,sizeof(int));
 
-  int* m3_c = copy(m1,NUM_ELEMS);
+  int* m1 = initRandomArrayi(NUM_ELEMS);
 
+  copy(m3_c,m1,NUM_ELEMS);
   dag_copy(m3_dag,m1,NUM_ELEMS);
 
-  verifyArrays("mmult",m3_c,m3_dag,len);
+  verifyArrays("copy",m3_c,m3_dag,NUM_ELEMS);
 }
